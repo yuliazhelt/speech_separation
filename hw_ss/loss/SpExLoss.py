86129d0e99898ca49353e2ef38fcbf7c7b319281
Ubuntu <juliazh2003@gmail.com>
2023-11-13 18:11:37 +0000

    update loss module

--- hw_ss/loss/SpExLoss.py
+++ hw_ss/loss/SpExLoss.py
@@ -1,10 +1,13 @@
 import torch
 from torch import Tensor
 from torch import nn
+from torchmetrics.audio import ScaleInvariantSignalDistortionRatio
+
 import torch.nn.functional as F
 
 
 def si_sdr(pred, target):
+
     alpha = (target * pred).sum() / torch.linalg.norm(target)**2
     return 20 * torch.log10(torch.linalg.norm(alpha * target) / (torch.linalg.norm(alpha * target - pred) + 1e-6) + 1e-6)
 
@@ -19,16 +22,17 @@ class SI_SDRLoss(nn.Module):
         return - ((1 - self.alpha - self.beta) * si_sdr(pred=s1, target=audio_target) + self.alpha * si_sdr(pred=s2, target=audio_target) + self.beta * si_sdr(pred=s3, target=audio_target))
     
 
-class SpExLoss(SI_SDRLoss):
-    def __init__(self, gamma, *args, **kwargs): 
+class SpExLoss(nn.Module):
+    def __init__(self, alpha, beta, gamma, *args, **kwargs): 
         super(SpExLoss, self).__init__(*args, **kwargs)
+        self.alpha = alpha
+        self.beta = beta
         self.gamma = gamma
+        self.si_sdr = ScaleInvariantSignalDistortionRatio().to(torch.cuda.current_device())
 
     def forward(self, s1: Tensor, s2: Tensor, s3: Tensor, audio_target: Tensor, logits: Tensor, speaker_id: Tensor, **batch) -> Tensor:
-        if s1 is None:
-            si_sdr_loss = 0
-        else:
-            si_sdr_loss = super().forward(s1, s2, s3, audio_target, **batch)
+        # si_sdr_loss = super().forward(s1, s2, s3, audio_target, **batch)
+        si_sdr_loss = - ((1 - self.alpha - self.beta) * self.si_sdr(s1, audio_target) + self.alpha * self.si_sdr(s2, audio_target) + self.beta * self.si_sdr(s3, audio_target))
         return si_sdr_loss + self.gamma * F.cross_entropy(logits, speaker_id)
 
     
